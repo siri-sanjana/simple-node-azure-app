42dfb824c06af030f529a13480ed52a7c7bf9c67
Siri Sanjana <sirisanjana@Siris-MacBook-Pro.local>
2025-07-16 14:23:29 +0530

    Done

--- api/hello.js
+++
@@ -1,7 +0,0 @@
-// api/hello.js
-module.exports = async function (context, req) {
-context.res = {
-headers: { "Content-Type": "application/json" },
-body: { message: "Hello from Azure API!" },
-};
-};
